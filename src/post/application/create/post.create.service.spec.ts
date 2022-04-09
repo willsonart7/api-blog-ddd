0cf575f3710cc6ea6efdc5399d73006e15828b71
Willson Arteaga <willsonart7@gmail.com>
2022-04-09 17:54:28 -0400

    🔴PostCreateService test created

---
+++ src/post/application/create/post.create.service.spec.ts
@@ -0,0 +1,47 @@
+import { Test } from '@nestjs/testing';
+import { SharedModule } from 'src/shared/shared.module';
+
+describe('Post', () => {
+
+    let postCreateService: PostCreateService;
+    let postMongoRepository: PostMongoRepository;
+
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [
+                SharedModule
+            ],
+            providers: [
+                PostCreateService,
+                {
+                    provide: PostMongoRepository,
+                    useValue: {
+                        save: () => { }
+                    },
+                },
+
+            ],
+        }).compile();
+        postCreateService = moduleRef.get<PostCreateService>(PostCreateService);
+        postMongoRepository = moduleRef.get<PostMongoRepository>(PostMongoRepository);
+    });
+
+    describe('create', () => {
+
+        it('should be save', async () => {
+
+            const id = '41b8316f-5e68-4b2d-8699-f9189de55399';
+            const name = 'Post test example';
+            const description = 'Post test example description';
+
+            jest.spyOn(postMongoRepository, 'save').getMockImplementation()
+
+            await postCreateService.execute(id, name, description);
+
+            expect(postMongoRepository.save).toBeCalled()
+        });
+
+    })
+
+
+})
